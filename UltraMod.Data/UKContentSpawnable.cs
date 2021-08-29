21884ea7080bdcfc9d91fb95623afc83191c464a
Dazegambler <dazegambler@gmail.com>
2021-08-28 21:51:56 -0300

    new branch lua

---
+++ UltraMod.Data/UKContentSpawnable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UltraMod.Data
+{
+    public enum Type
+    {
+        Enemy,
+        Spawnable,
+    }
+
+    [CreateAssetMenu(fileName = "UKContentWeapon", menuName = "UltraMod/UKContentSpawnable")]
+    class UKContentSpawnable : ScriptableObject
+    {
+        public Type type;
+        public string Name;
+
+        public Sprite Icon;
+
+        public GameObject Prefab;
+    }
+}
---
+++ UltraMod.Data/UKContentWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UltraMod.Data
+{
+    [CreateAssetMenu(fileName = "UKContentWeapon",menuName = "UltraMod/UKContentWeapon")]
+    class UKContentWeapon : ScriptableObject
+    {
+        public string Name;
+
+        public Sprite Icon;
+
+        public List<GameObject> Variants;
+    }
+}
