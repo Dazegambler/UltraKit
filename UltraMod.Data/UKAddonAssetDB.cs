ef1572bc706e97a91b86e0245347c3998b6d9198
Hypatech <silverma@hotmail.co.uk>
2021-08-29 20:43:52 +0100

    Change data project + some other changes

---
+++ UltraMod.Data/UKAddonAssetDB.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace UltraMod.Data
+{
+    [Serializable]
+    public struct UKAddonAsset
+    {
+        public string Name;
+        public UnityEngine.Object asset;
+    }
+
+    [CreateAssetMenu(fileName = "New Asset Database", menuName = "UltraMod/Asset Database")]
+    public class UKAddonAssetDB : ScriptableObject
+    {
+        List<UKAddonAsset> Assets;
+    }
+}
