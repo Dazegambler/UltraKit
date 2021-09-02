6c0e81a02703d9014255d23ab5e7d60474e0e8d9
Hypatech <silverma@hotmail.co.uk>
2021-09-02 19:39:43 +0100

    Set major assists as on

---
+++ ULTRAKIT.Loader/Injectors/CyberGrindInjector.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULTRAKIT.Loader.Injectors
+{
+    [HarmonyPatch(typeof(AssistController))]
+    public static class AssistControllerPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void SetInfoPost(AssistController __instance)
+        {
+            __instance.majorEnabled = true;
+        }
+    }
+}
