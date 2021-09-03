5540e83faf8afdfc9828cf4363c03450b0da1630
Hypatech <silverma@hotmail.co.uk>
2021-09-03 18:51:01 +0100

    Move boss spawns to new folder

---
+++ ULTRAKIT.Core/BossSpawns/BossSpawnsInjector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ULTRAKIT.Data.ScriptableObjects.Registry;
+using UnityEngine;
+
+namespace ULTRAKIT.Core.BossSpawns
+{
+    public static class BossSpawnsInjector
+    {
+        public static void Initialize()
+        {
+            Loader.Addon b = new Loader.Addon();
+
+            b.Data = new Data.UKAddonData();
+            b.Data.ModName = "Vanilla Bosses/Enemies";
+            b.Data.Author = "UltraKit";
+            b.Data.ModDesc = "Contains Enemies that cannot be spawned by the spawner arm";
+
+            b.Bundle = ModMenu.ModMenuInjector.UIBundle;///SINCE EVERYTHING WILL BE REGISTERED OUTSIDE OF THE ADDON I'LL JUST USE THE UI BUNDLE
+
+            b.Path = "Internal";
+
+            b.enabled = true;
+
+            Loader.AddonLoader.registry.Add(b, new List<UKContent>());
+            Loader.AddonLoader.registry[b].AddRange(Enemies());
+        }
+
+        public static List<UKContentSpawnable> Enemies()
+        {
+            List<UKContentSpawnable> a = new List<UKContentSpawnable>();
+            string[] List ={
+                "MinosPrime",
+                "V2",
+                "Gabriel",
+                "DroneFlesh",
+                "Flesh Prison",
+                "MinosBoss",
+                "Wicked",
+                "DroneSkull Variant"
+            };
+
+            foreach (string _List in List) a.Add(EnemySpawnable(_List));
+
+            return a;
+        }
+        public static UKContentSpawnable EnemySpawnable(string Enemy)
+        {
+
+            UKContentSpawnable a = ScriptableObject.CreateInstance<UKContentSpawnable>();
+
+            a.Name = Enemy;
+            a.type = Data.ScriptableObjects.Registry.Type.Enemy;
+
+            a.Prefab = PrefabFind(Enemy);
+
+            return a;
+        }
+        public static GameObject PrefabFind(string name)
+        {
+            //Find set Object in the prefabs
+            GameObject[] Pool = Resources.FindObjectsOfTypeAll<GameObject>();
+            GameObject a = null;
+            foreach (GameObject obj in Pool)
+            {
+                if (obj.gameObject.name == name)
+                {
+                    if (obj.gameObject.tag == "Enemy" || name == "Wicked")
+                    {
+                        if (obj.activeSelf != true) obj.SetActive(true);
+                        a = obj;
+
+                        // Fix lighting
+                        var smrs = a.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+                        foreach (var item in smrs)
+                        {
+                            item.gameObject.layer = LayerMask.NameToLayer("Outdoors");
+                        }
+                    }
+                }
+            }
+
+
+            return a;
+        }
+    }
+}
--- ULTRAKIT.Core/CoreContent.cs
+++ ULTRAKIT.Core/CoreContent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ULTRAKIT.Core.BossSpawns;
 using ULTRAKIT.Core.ModMenu;
 using ULTRAKIT.Data.ScriptableObjects.Registry;
 using ULTRAKIT.Loader.Registries;
@@ -15,71 +16,8 @@ namespace ULTRAKIT.Core
         public static void Initialize()
         {
             ModMenuInjector.Initialize();
-            CreateBossAddon();
+            BossSpawnsInjector.Initialize();
         }
-        public static void CreateBossAddon()
-        {
-            Loader.Addon b = new Loader.Addon();
-
-            b.Data = new Data.UKAddonData();
-            b.Data.ModName = "<size=9>Vanilla Bosses/Enemies</size>";
-            b.Data.Author = "UltraKit";
-            b.Data.ModDesc = "Contains Enemies that cannot be spawned by the spawner arm";
-
-            b.Bundle = ModMenu.ModMenuInjector.UIBundle;///SINCE EVERYTHING WILL BE REGISTERED OUTSIDE OF THE ADDON I'LL JUST USE THE UI BUNDLE
-
-            b.Path = "Internal";
-
-            b.enabled = true;
-
-            Loader.AddonLoader.registry.Add(b, new List<UKContent>());
-            Loader.AddonLoader.registry[b].AddRange(Enemies());
-            //Loader.AddonLoader.registry[b].AddRange(new List<UKContentWeapon>());
-        }
-        public static List<UKContentSpawnable> Enemies()
-        {
-            List<UKContentSpawnable> a = new List<UKContentSpawnable>();
-            string[] List ={
-                "MinosPrime",
-                "V2",
-                "Gabriel",
-                "DroneFlesh",
-                "FleshPrison",
-                "MinosBoss",
-                "Wicked",
-                "DroneSkull Variant"
-            };
-
-            foreach (string _List in List) a.Add(EnemySpawnable(_List));
-
-            return a;
-        }
-        public static  UKContentSpawnable EnemySpawnable(string Enemy)
-        {
-
-            UKContentSpawnable a = new UKContentSpawnable();
-
-            a.Name = Enemy;
-            a.type = Data.ScriptableObjects.Registry.Type.Enemy;
-
-            a.Prefab = PrefabFind(Enemy);
-
-            return a;
-        }
-        public static GameObject PrefabFind(string name)
-        {
-            //Find set Object in the prefabs
-            GameObject[] Pool = Resources.FindObjectsOfTypeAll<GameObject>();
-            GameObject a = new GameObject();
-            foreach (GameObject obj in Pool)
-            {
-                if (obj.gameObject.name == name && obj.gameObject.tag == "Enemy")
-                {
-                    if (obj.activeSelf != true) obj.SetActive(true);
-                    a = obj;
-                }
-            }
-            return a;
-        }
+        
     }
 }
