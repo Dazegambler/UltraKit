05d57e415f381ef415d889772944c4c8e5f3b814
Dazegambler <dazegambler@gmail.com>
2021-09-11 23:17:41 -0300

    wip

--- ULTRAKIT.Lua/API/Proxies/Components/Unity/UKProxyMeshFilter.cs
+++ ULTRAKIT.Lua/API/Proxies/Components/Unity/UKProxyMeshFilter.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace ULTRAKIT.Lua.API.Proxies.Components.Unity
 {
-    class UKProxyMeshFilter
+    public class UKProxyMeshFilter : UKProxyComponentAbstract<MeshFilter>
     {
+        public UKProxyMeshFilter(MeshFilter target) : base(target)
+        {
+        }
+        public Mesh mesh
+        {
+            get => target.mesh;
+            set => target.mesh = value;
+        }
+        public Mesh sharedMesh
+        {
+            get => target.sharedMesh;
+            set => target.sharedMesh = value;
+        }
     }
 }
