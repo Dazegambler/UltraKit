797d9f4bb08ab0c831d402f53994dc31693f02e2
Hypatech <silverma@hotmail.co.uk>
2021-09-16 20:43:37 +0100

    Animator proxy

--- ULTRAKIT.Lua/API/Proxies/Components/Game/UKProxyProjectile.cs
+++ ULTRAKIT.Lua/API/Proxies/Components/Game/UKProxyProjectile.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using MoonSharp.Interpreter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace ULTRAKIT.Lua.API.Proxies.Components
 {
     public class UKProxyProjectile : UKProxyComponentAbstract<Projectile>
     {
+        [MoonSharpHidden]
         public UKProxyProjectile(Projectile target) : base(target)
         {
         }
+
+        public Rigidbody rigidbody => target.GetComponent<Rigidbody>();
     }
 }
