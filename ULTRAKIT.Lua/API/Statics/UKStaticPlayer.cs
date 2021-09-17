7cb6ffd0a0547c0cc7bad7fb7cd32e834d0d0657
Hypatech <silverma@hotmail.co.uk>
2021-09-17 18:20:32 +0100

    Update UKStaticPlayer.cs

--- ULTRAKIT.Lua/API/Statics/UKStaticPlayer.cs
+++ ULTRAKIT.Lua/API/Statics/UKStaticPlayer.cs
@@ -21,5 +21,7 @@ namespace ULTRAKIT.Lua.API.Statics
             get => MonoSingleton<GunControl>.Instance.enabled;
             set => MonoSingleton<GunControl>.Instance.enabled = value;
         }
+        
+        public void CameraShake(float amount) => MonoSingleton<CameraController>.Instance.CameraShake(amount);
     }
 }
