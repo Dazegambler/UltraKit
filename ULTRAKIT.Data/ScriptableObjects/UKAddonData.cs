a90ba07f6c3469fcd8f9f0b174ee1681275208a7
Hypatech <silverma@hotmail.co.uk>
2021-09-17 17:35:21 +0100

    Browse button for path

--- ULTRAKIT.Data/ScriptableObjects/UKAddonData.cs
+++ ULTRAKIT.Data/ScriptableObjects/UKAddonData.cs
@@ -3,13 +3,10 @@ using UnityEngine;
 namespace ULTRAKIT.Data
 {
     public class UKAddonData : ScriptableObject
-    {
+    {   
         public string ModName;
         public string Author;
-
         [TextArea]
         public string ModDesc;
-
-        public string ExportPath;
     }
 }
