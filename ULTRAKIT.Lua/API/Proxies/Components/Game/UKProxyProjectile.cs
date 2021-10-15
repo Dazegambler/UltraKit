eeb1a168ae151c9507b1cadc6e36b2a1b87ab41b
NotABot <noah.tisdale.hall@gmail.com>
2021-10-16 02:47:03 +1100

    projectile proxy is no longer boarderline useless

--- ULTRAKIT.Lua/API/Proxies/Components/Game/UKProxyProjectile.cs
+++ ULTRAKIT.Lua/API/Proxies/Components/Game/UKProxyProjectile.cs
@@ -15,6 +15,129 @@ namespace ULTRAKIT.Lua.API.Proxies.Components
         {
         }
 
+        public GameObject explosionEffect
+        {
+            get => target.explosionEffect;
+            set => target.explosionEffect = value;
+        }
+
+        public float damage
+        {
+            get => target.damage;
+            set => target.damage = value;
+        }
+        
+        public float speed
+        {
+            get => target.speed;
+            set => target.speed = value;
+        }
+
+        public float speedRandomizer
+        {
+            get => target.speedRandomizer;
+            set => target.speedRandomizer = value;
+        }
+
+        public float turningSpeedMultiplier
+        {
+            get => target.turningSpeedMultiplier;
+            set => target.turningSpeedMultiplier = value;
+        }
+
+        public bool undeflectable
+        {
+            get => target.undeflectable;
+            set => target.undeflectable = value;
+        }
+
+        public bool decorative
+        {
+            get => target.decorative;
+            set => target.decorative = value;
+        }
+
+        public bool canHitCoin
+        {
+            get => target.canHitCoin;
+            set => target.canHitCoin = value;
+        }
+
+        public Transform targetTransform
+        {
+            get => target.target;
+            set => target.target = value;
+        }
+
+
+
+        public bool friendly
+        {
+            get => target.friendly;
+            set => target.friendly = value;
+        }
+        
+        public string bulletType
+        {
+            get => target.bulletType;
+            set => target.bulletType = value;
+        }
+
+        public string weaponType
+        {
+            get => target.weaponType;
+            set => target.weaponType = value;
+        }
+
+        public bool explosive
+        {
+            get => target.explosive;
+            set => target.explosive = value;
+        }
+
+        public bool bigExplosion
+        {
+            get => target.bigExplosion;
+            set => target.bigExplosion = value;
+        }
+
+        public bool playerBullet
+        {
+            get => target.playerBullet;
+            set => target.playerBullet = value;
+        }
+
+        public string homingType
+        {
+            get => target.homingType.ToString();
+            set => target.homingType = setHomingType(value);
+        }
+
         public Rigidbody rigidbody => target.GetComponent<Rigidbody>();
+
+        public HomingType setHomingType(string type)
+        {
+            if (type == "Gradual")
+            {
+                return HomingType.Gradual;
+            }
+            if (type == "HorizontalOnly")
+            {
+                return HomingType.HorizontalOnly;
+            }
+            if (type == "Instant")
+            {
+                return HomingType.Instant;
+            }
+            if (type == "Loose")
+            {
+                return HomingType.Loose;
+            }
+            if (type == "None")
+            {
+                return HomingType.None;
+            }
+            return HomingType.None;
+        }
     }
 }
