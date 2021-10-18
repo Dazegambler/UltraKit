01b0d121b67498cc7e10f27ec6eb8736ae73ee0d
PetersonE1 <PetersonE_1@live.com>
2021-10-17 22:56:08 -0400

    added UKProxyCheatsController
    added a new proxy for the cheats controller, which can currently only access the bools for blinded enemies, enemy spawning, and weapon cooldown.

---
+++ ULTRAKIT.Lua/API/Proxies/Components/Game/UKProxyCheatsController.cs
@@ -0,0 +1,21 @@
+﻿using MoonSharp.Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ULTRAKIT.Lua.API.Proxies.Components
+{
+    public class UKProxyCheatsController : UKProxyComponentAbstract<CheatsController>
+    {
+        [MoonSharpHidden]
+        public UKProxyCheatsController(CheatsController target) : base(target)
+        {
+        }
+        public bool BlindEnemies = CheatsController.BlindEnemies;
+        public bool IgnoreArenaTriggers = CheatsController.IgnoreArenaTriggers;
+        public bool NoCooldown = CheatsController.NoCooldown;
+    }
+}
